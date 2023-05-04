dcecd3f4048959fcfdad6b9c4af5928d1012a6f5
makDmitr <130734119+makDmitr@users.noreply.github.com>
2023-05-04 09:53:23 +0400

    Created Data Layer
    Provided Repository Implementation

--- app/src/main/java/com/example/numbercomposition/domain/entities/Question.kt
+++ app/src/main/java/com/example/numbercomposition/domain/entities/Question.kt
@@ -1,10 +1,10 @@
 package com.example.numbercomposition.domain.entities
 
 data class Question(
-    val answer: Int,
+    val sum: Int,
     val visibleDigit: Int,
     val options: List<Int>
 ) {
     val invisibleDigit: Int
-        get() = answer - visibleDigit
+        get() = sum - visibleDigit
 }
